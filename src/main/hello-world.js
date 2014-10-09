22dfa743cff3476a1d435b857c1d662d427799db
Hao Hu <hhu@thoughtworks.com>
2014-10-09 10:48:07 +0800

    add test

---
+++ src/main/hello-world.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.beenCalled = function() {
+  return 'Leave me alone.';
+};
---
+++ src/test/hello-world-spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var expect = require('chai').expect;
+// var HelloWorld = require('../main/hello-world');
+
+describe('HelloWorld', function() {
+
+  it('hello world test', function(){
+    expect('Hello World').to.equal('Hello World');
+  });
+
+  it('should be mock');
+});
