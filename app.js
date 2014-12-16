760204bf472b02cc5bb91c910c2b9d4fbe9025d5
Hao Hu <hhu@thoughtworks.com>
2014-12-16 11:05:20 +0800

    remove all semicolon

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-'use strict';
+'use strict'
 
-var readline = require('readline');
+var readline = require('readline')
 
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
 // This is an example for reading & writing in console.
 rl.question('What is your name? ', function(answer) {
-
-  console.log('Hello ' + answer + '!');
-
-  rl.close();
-});
+  console.log('Hello ' + answer + '!')
+  rl.close()
+})
--- src/test/hello-world-spec.js
+++ src/test/hello-world-spec.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-var sinon = require('sinon');
-var expect = require('chai').expect;
-var HelloWorld = require('../main/hello-world').HelloWorld;
+var sinon = require('sinon')
+var expect = require('chai').expect
+var HelloWorld = require('../main/hello-world').HelloWorld
 
 describe('HelloWorld', function() {
 
   it('demo test', function() {
-    expect('Hello World').to.equal('Hello World');
-  });
+    expect('Hello World').to.equal('Hello World')
+  })
 
   it('should be mock', function() {
     var helloWorld = sinon.createStubInstance(HelloWorld);
-    helloWorld.beenCalled.returns('Hello World');
+    helloWorld.beenCalled.returns('Hello World')
 
-    expect(helloWorld.beenCalled()).to.equal('Hello World');
-  });
-});
+    expect(helloWorld.beenCalled()).to.equal('Hello World')
+  })
+})
